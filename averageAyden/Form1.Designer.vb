c44f6a5b432b4d4b8397d4ccad16a00b20236a96
AYDEN HOLGATE <AYDEN.HOLGATE@connect.7oaks.org>
2021-01-25 14:40:46 -0600

    Initial commit

---
+++ averageAyden/Form1.Designer.vb
@@ -0,0 +1,114 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblTitle = New System.Windows.Forms.Label()
+        Me.txtNumber = New System.Windows.Forms.TextBox()
+        Me.lblNumber = New System.Windows.Forms.Label()
+        Me.btnAverage = New System.Windows.Forms.Button()
+        Me.lblAverage = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'lblTitle
+        '
+        Me.lblTitle.AutoSize = True
+        Me.lblTitle.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblTitle.Location = New System.Drawing.Point(0, 0)
+        Me.lblTitle.Name = "lblTitle"
+        Me.lblTitle.Size = New System.Drawing.Size(129, 31)
+        Me.lblTitle.TabIndex = 0
+        Me.lblTitle.Text = "Averages"
+        '
+        'txtNumber
+        '
+        Me.txtNumber.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtNumber.Location = New System.Drawing.Point(165, 105)
+        Me.txtNumber.Name = "txtNumber"
+        Me.txtNumber.Size = New System.Drawing.Size(174, 29)
+        Me.txtNumber.TabIndex = 1
+        '
+        'lblNumber
+        '
+        Me.lblNumber.AutoSize = True
+        Me.lblNumber.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblNumber.Location = New System.Drawing.Point(12, 105)
+        Me.lblNumber.Name = "lblNumber"
+        Me.lblNumber.Size = New System.Drawing.Size(147, 24)
+        Me.lblNumber.TabIndex = 2
+        Me.lblNumber.Text = "Enter A Number"
+        '
+        'btnAverage
+        '
+        Me.btnAverage.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnAverage.Location = New System.Drawing.Point(12, 234)
+        Me.btnAverage.Name = "btnAverage"
+        Me.btnAverage.Size = New System.Drawing.Size(184, 37)
+        Me.btnAverage.TabIndex = 3
+        Me.btnAverage.Text = "Calculate Average"
+        Me.btnAverage.UseVisualStyleBackColor = True
+        '
+        'lblAverage
+        '
+        Me.lblAverage.AutoSize = True
+        Me.lblAverage.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblAverage.Location = New System.Drawing.Point(12, 321)
+        Me.lblAverage.Name = "lblAverage"
+        Me.lblAverage.Size = New System.Drawing.Size(101, 24)
+        Me.lblAverage.TabIndex = 4
+        Me.lblAverage.Text = "Average: 0"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(16, 410)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 5
+        Me.Button1.Text = "Clear"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(388, 445)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.lblAverage)
+        Me.Controls.Add(Me.btnAverage)
+        Me.Controls.Add(Me.lblNumber)
+        Me.Controls.Add(Me.txtNumber)
+        Me.Controls.Add(Me.lblTitle)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents lblTitle As System.Windows.Forms.Label
+    Friend WithEvents txtNumber As System.Windows.Forms.TextBox
+    Friend WithEvents lblNumber As System.Windows.Forms.Label
+    Friend WithEvents btnAverage As System.Windows.Forms.Button
+    Friend WithEvents lblAverage As System.Windows.Forms.Label
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+
+End Class
---
+++ averageAyden/Form1.vb
@@ -0,0 +1,21 @@
+﻿Public Class Form1
+    Dim sngTotal As Single
+    Dim intCounter As Integer = 0
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnAverage.Click
+        Dim sngAverage As Single
+
+        sngTotal = sngTotal + Val(txtNumber.Text)
+
+        intCounter = intCounter + 1
+        sngAverage = sngTotal / intCounter
+
+        lblAverage.Text = "Average: " & sngAverage
+    End Sub
+        
+    Private Sub Button1_Click_1(sender As Object, e As EventArgs) Handles Button1.Click
+        sngTotal = 0
+        lblAverage.Text = "Average: " & sngTotal
+
+    End Sub
+End Class
